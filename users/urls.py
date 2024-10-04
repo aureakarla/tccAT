d031bb326028fecdfbc48418db358a37c4eeaf09
Áurea <aurea.b@escolar.ifrn.edu.br>
2024-10-04 14:04:36 -0300

    Limpar cache

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import cadastro, logout_, login_
-
-urlpatterns = [
-    path('accounts/cadastro/', cadastro, name='cadastro'),
-    path('accounts/login/', login_, name='login'),
-    path('accounts/logout/', logout_, name='logout'),
-]
